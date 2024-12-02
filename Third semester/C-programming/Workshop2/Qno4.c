f1189da55f8a83a30c7b886259db2c049ec417de
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2024-12-02 19:36:26 +0545

    updated qno4

--- Third semester/C-programming/Workshop2/Qno4.c
+++ Third semester/C-programming/Workshop2/Qno4.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
 #include <math.h>
-// Function to calculate velocity based on the formula v = u + at
-double velocityCalc(double u, double a, float t) {
-    if (isnan(u)) {
-        return a * t;
-    } else if (isnan(a)) {
-        return (u / t);
-    } else if (isnan(t)) {
-        return (u + a);
-    } else {
+
+double velocityCalc(double v, double u, double a, double t) {
+    if (isnan(v)) {
         return u + a * t;
+    } else if (isnan(u)) {
+       return v - a * t;
+    } else if (isnan(a)) {
+       return (v - u) / t;
+   } else if (isnan(t)) {
+        return (v - u) / a;
+    } else {
+        return NAN;
     }
 }
-int main() {
-    double u = 21;
-    double a = 32;
-    float t = 32.3;
-    printf("Final velocity: %f\n", velocityCalc(u, a, t));
-    return 0;
+int main(){
+   double v = velocityCalc(0, NAN, 9.8, 2);
+   printf("Calculated velocity: %.2f\n", v);
+   return 0;
 }
